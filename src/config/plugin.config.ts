a06528e20ac02a80349b66756f63aa8ab1934a35
zhangjiajia04 <zhangjiajia04@58.com>
2024-11-21 17:38:09 +0800

    test

--- src/config/plugin.config.ts
+++ src/config/plugin.config.ts
@@ -1,9 +1,9 @@
-import Uma, { TPluginConfig } from '@umajs/core';
+import Uma, { TConfig } from '@umajs/core';
 import * as path from 'path';
 
 const { ROOT } = Uma.options;
 
-export default <{ [key: string]: TPluginConfig }>{
+export default <TConfig>{
     views: {
         enable: true,
         name: 'views',
@@ -36,12 +36,6 @@ export default <{ [key: string]: TPluginConfig }>{
             infoLogName: 'infologger.log', // info级别日志文件名
             warnLogName: 'warnlogger.log', // warn级别日志文件名
             allowDebugAtProd: true, // 是否允许打印debug日志
-        },
-    },
-    static: {
-        options: {
-            root: './static',
-            opts: {},
         },
     },
 };
--- web/pages/router/home.js
+++ web/pages/router/home.js
@@ -5,14 +5,25 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
         this.goAboutPage = this.goAboutPage.bind(this);
+        this.goAboutPageByQuery = this.goAboutPageByQuery.bind(this);
     }
 
     goAboutPage() {
         this.props.history.push({
             pathname: '/about',
             state: {
-                msg: '来自首页的问候！by state',
+                msg: '来自首页的问候！by state', // 页面刷新后依然保持
             },
+        });
+    }
+
+    goAboutPageByQuery() {
+        this.props.history.push({
+            pathname: '/about',
+            query: {
+                msg: '我是js query url参数', // query查询参数不表现在URL上 页面刷新后消失
+            },
+            search: '?query=aaa', // 携带到URL上面的search
         });
     }
 
@@ -25,6 +36,7 @@ export default class Home extends Component {
                 <br />
                 <Link to="/about/我是url参数">去关于我的页面 路由配置传递参数</Link>
                 <div onClick={this.goAboutPage}>去关于我的页面 js方式state传递参数</div>
+                <div onClick={this.goAboutPageByQuery}>去关于我的页面 js方式query传递参数</div>
             </div>
         );
     }
